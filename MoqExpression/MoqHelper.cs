fccc2cca5eaef47f5c518b5fd33c246edec53992
Ovais Hanif <ovaishanif94@gmail.com>
2019-03-30 20:22:32 +0500

    Write lambda expression and moq expression helpers

---
+++ MoqExpression/MoqHelper.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using System;
+using System.Linq.Expressions;
+
+namespace MoqExpression
+{
+    public static class MoqHelper
+    {
+        public static Expression<Func<T, bool>> IsExpression<T>(Expression<Func<T, bool>> query)
+        {
+            return It.Is<Expression<Func<T, bool>>>(expression => LambdaCompare.Eq(expression, query));
+        }
+    }
+}
