c1516d8cc55ca325591b6e823f583b13c13ffb91
Ovais Hanif <ovaishanif94@gmail.com>
2019-04-04 22:20:51 +0500

    Add comments on helper function

--- MoqExpression/MoqHelper.cs
+++ MoqExpression/MoqHelper.cs
@@ -6,6 +6,13 @@ namespace MoqExpression
 {
     public static class MoqHelper
     {
+        /// <summary>
+        /// Match expression function methods with lambda function.
+        /// https://github.com/ovaishanif94/Moq.Expression
+        /// </summary>
+        /// <typeparam name="T">Type of class</typeparam>
+        /// <param name="query">Parameters to match</param>
+        /// <returns></returns>
         public static Expression<Func<T, bool>> IsExpression<T>(Expression<Func<T, bool>> query)
         {
             return It.Is<Expression<Func<T, bool>>>(expression => LambdaCompare.Eq(expression, query));
